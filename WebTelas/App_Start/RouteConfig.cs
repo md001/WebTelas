5b751f40fb704e9e48da4df3037db6676d21c29c
Mario GRANADOS <mariodidier@gmail.com>
2018-06-15 17:22:40 -0500

    Agregar archivos de proyecto.

---
+++ WebTelas/App_Start/RouteConfig.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace WebTelas
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                // controller = Views\"<Nombre>" action = Views\Nombre\"Nombre".cshtml
+                defaults: new { controller = "Dashboard", action = "Home", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ WebTelas/Controllers/DashboardController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebTelas.Controllers
+{
+    public class DashboardController : Controller
+    {
+        // GET: Dashboard
+        public ActionResult Home() // Views\Dashboard\"Home"
+        {
+            return View();
+        }
+    }
+}
